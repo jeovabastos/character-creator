1138ac58fb138224958b9e4e40160580e6daf30b
jeovabastos <jbastos.im@gmail.com>
2024-03-20 17:16:43 -0400

    feat: agora o user pode adicionar outras propriedades

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 import { listOptions } from "./lists.js"
 import { Character } from "./character.js"
 
+// testing addProperty
+function newProperty(title){
+    if(title !== ''){
+        const testDiv = document.createElement('div')
+        const testTitle = document.createElement('h2')
+        const testInput = document.createElement('input')
+        
+        testTitle.innerHTML = title
+        testInput.type = 'text'
+        testInput.id = title
+        inputsArray.push({id:testInput.id, property: testInput.id})
+        
+        testDiv.appendChild(testTitle)
+        testDiv.appendChild(testInput)
+        
+        const testArea = document.getElementById('properties')
+        testArea.append(testDiv)
+    }
+    return
+}
+document.getElementById('addPropertyButton').addEventListener('click', ()=>{
+    const input = document.getElementById('addPropertyInput')
+    newProperty(input.value)
+    input.value = ''
+
+    pegarInputsManuais()
+})
+
+
 function createCharacter(object){
     return new Character(object)
 }
-
 const startCharacter = createCharacter(listOptions)
 
+// INPUTS DO SISTEMA
 const inputsArray = [
     {id: 'charName', property: 'name'},
     {id: 'charCity', property: 'city'},
@@ -14,19 +43,30 @@ const inputsArray = [
     {id: 'charAge', property: 'age'}
 ]
 
-const currentlyCharacter = {
-    name: document.getElementById('charName'),
-    city: document.getElementById('charCity'),
-    nationality: document.getElementById('charNationality'),
-    age: document.getElementById('charAge')
+function turnIntoProperty(list){
+    const momentum = {}
+    list.map(item=>{
+        momentum[item.property] = document.getElementById(item.id)
+    })
+    return momentum
 }
+const currentlyCharacter = turnIntoProperty(inputsArray)
 
+
+
+
+
+
+// PEGAR OS INPUTS GERADOS (AUTOMATICAMENTE)
 document.getElementById('generatorButton').addEventListener('click', ()=>{
     const character = startCharacter.default()
     console.log('default: ', character)
     
     function alterarValorInput(id, property){
-        document.getElementById(id).value = character[property]
+        if(character[property]){
+            document.getElementById(id).value = character[property]
+        }
+        return
     }
 
     inputsArray.map(item =>{
@@ -43,18 +83,32 @@ document.getElementById('generatorButton').addEventListener('click', ()=>{
     return
 })
 
+
+
+
+
 const userInputs = {}
 
-// PEGAR OS INPUTS DO USUARIO
+// PEGAR OS INPUTS DO USUARIO (MANUALMENTE)
 function addInputEvent(id, property){    
     document.getElementById(id).addEventListener('input', (e)=>{ 
         userInputs[property] = `# ${property} \n ${e.target.value} \n\n`
     })
 }    
-inputsArray.map(item=>{
-    addInputEvent(item.id, item.property)
-})
 
+function pegarInputsManuais(){
+    inputsArray.map(item=>{
+        addInputEvent(item.id, item.property)
+    })
+}
+
+pegarInputsManuais()
+
+
+
+
+
+// SALVAR O ARQUIVO (EM FORMATO .MD)
 function downloadTextAsFile(filename) {
     const data = Object.values(userInputs)
     let parsedData = data[0]
@@ -76,13 +130,25 @@ document.getElementById('saveFileButton').addEventListener('click', ()=>{
     downloadTextAsFile('meu_arquivo.md')
 })
 
-// TESTANDO FEATURE NOVO INPUT
+
+
+
+// NOVOS INPUTS
+let newInputsArray = [
+    {id: 'newNameButton', property: 'name'},
+    {id: 'newCityButton', property: 'city'},
+    {id: 'newNationalityButton', property: 'nationality'},
+    {id: 'newAgeButton', property: 'age'},
+]
+
 function newInputValue(id, property){
     document.getElementById(id).addEventListener('click', ()=>{
         const character = startCharacter.default()
-        console.log(`${property} AGAIN: `, character[property])
+        
         currentlyCharacter[property].value = character[property]
-        userInputs.name = `# ${property} \n ${character[property]} \n\n`
+        userInputs[property] = `# ${property} \n ${character[property]} \n\n`
     })
 }
-newInputValue('newNameButton', 'name')
+newInputsArray.map(item=>{
+    return newInputValue(item.id, item.property)
+})
